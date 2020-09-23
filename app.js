4c89964ca0b2410f20868e78d0ea6efb5700818a
Torben Jan Müller <torben.jan.mueller@gmail.com>
2020-09-23 17:14:44 +0200

    Second commit

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ app.use(express.static("public"));
 
 const port = 3000;
 
-const addresses = [];
+const addresses = [
+    {
+        id: 0,
+        name: "Max Mustermann",
+        phone: 1234567890
+    }
+];
 
-
+let index = 1;
 
 // Get methods
 app.get("/", (req, res) => {
@@ -27,20 +33,32 @@ app.get("/add-address", (req, res) => {
 // Post methods
 app.post("/", function(req, res) {
 
+    const id = index;
     const name = req.body.newName;
     const phone = req.body.newNumber;
+
     const newEntry = {
+        id: id,
         name: name,
         phone: phone
     };
     addresses.push(newEntry);
+    index++;
     res.redirect("/");
     
 });
 
 app.post("/delete", function(req, res) {
-    const addressId = req.body.checkbox;
-    console.log(addressId);
+    const addressId = req.body.addressId;
+
+    for (let i = 0; i < addresses.length; i++) {
+        let obj = addresses[i];
+    
+        if (addressId.indexOf(obj.id) !== -1) {
+            addresses.splice(i, 1);
+        }
+    }
+
     res.redirect("/");
 });
 
